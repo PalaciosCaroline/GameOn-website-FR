e78dce7b6176907db651670ab2a125a560daaf2a
PalaciosCaroline <caroline2807@yahoo.fr>
2022-07-22 08:16:07 +0200

    Form : mise en place des messages d' erreurs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -24,3 +24,18 @@ function launchModal() {
 modalBtnClose.onclick = () => {
   modalbg.style.display = "none";
 };
+
+//Eviter propagation evenement
+document.getElementById("entry").addEventListener("submit", function(event){
+  event.preventDefault();
+});
+
+
+//message d'erreur (en opacity = 0 avant ('data-error-visible',true))
+firstName.parentNode.setAttribute('data-error','Veuillez entrer 2 caractères ou plus pour le champ du prénom.');
+lastName.parentNode.setAttribute('data-error','"Veuillez entrer 2 caractères ou plus pour le champ du nom."');
+email.parentNode.setAttribute('data-error','l\'adresse email doit être valide');
+birthdate.parentNode.setAttribute('data-error','Vous devez entrer votre date de naissance et avoir au minimum 12ans.');
+quantity.parentNode.setAttribute('data-error','Une valeur numérique doit être saisie');
+location1.parentNode.setAttribute('data-error','Vous devez choisir une option.');
+checkbox1.parentNode.setAttribute('data-error','Vous devez accepter les termes et conditions.');
